44791f573fa4d8b3a29bb95d66631f0ad75bba54
claratanudy <clara.535200025.stu.untar.ac.id>
2024-01-18 14:08:35 +0700

    rename file

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,15 +2,17 @@ import Table from './message'; // Adjust the path based on your project structur
 
 const VehicleManagementCard = () => {
   return (
-    <div className="card">
-      <div className="card-header text-center" style={{ backgroundColor: '#80dcac', color: 'black' }}>
-        <h5>Vehicle Management</h5>
-      </div>
-      <div>
-        <button className="btn btn-primary float-right border-0 mt-4" style={{ backgroundColor: '#80dcac', color: 'black', borderRadius: '20px', float: 'right', marginRight: '18px' }}>+ New Vehicle</button>
-      </div>
-      <div className="card-body">
-        <Table />
+    <div className="" style={{ margin: ' margin: 250px auto'}}>
+      <div className="card">
+        <div className="card-header text-center" style={{ backgroundColor: '#80dcac', color: 'black' }}>
+          <h5>Vehicle Management</h5>
+        </div>
+        <div>
+          <button className="btn btn-primary float-right border-0 mt-4" style={{ backgroundColor: '#80dcac', color: 'black', borderRadius: '20px', float: 'right', marginRight: '18px' }}>+ New Vehicle</button>
+        </div>
+        <div className="card-body">
+          <Table />
+        </div>
       </div>
     </div>
   );
